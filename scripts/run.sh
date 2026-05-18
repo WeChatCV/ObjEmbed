4bd497a1d58da5ad08cca1b9beba39cd3cbf11e3
fushh <71886569+fushh@users.noreply.github.com>
2026-05-18 20:09:24 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 
 OUTPUT_DIR=output
-OUTPUT_DIR_FT=${OUTPUT_DIR}/exp6
+OUTPUT_DIR_FT=${OUTPUT_DIR}/ObjEmbed-2B-32cards
 mkdir -p ${OUTPUT_DIR_FT}
 
 
@@ -17,12 +17,13 @@ torchrun --nproc_per_node 8 \
     --master_addr=$MASTER_ADDR \
     --master_port=$PORT \
     train.py \
-    --output_dir "./log/Qwen3-VL-4B-exp6-16cards" \
-    --model_name_or_path "Qwen3-VL-4B-exp1-stage2" \
+    --output_dir "./log/ObjEmbed-2B-32cards" \
+    --model_name_or_path "fushh7/WeDetect-Ref-2B-stage2" \
+    --dataset_path "YOUR_DATASET_PATH"
     --dataset_name "mixture_with_caption" \
     --deepspeed scripts/zero2.json \
     --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 2 \
     --learning_rate 2e-5 \
     --warmup_ratio 0.05 \
     --logging_steps 1 \
@@ -30,7 +31,7 @@ torchrun --nproc_per_node 8 \
     --report_to none \
     --gradient_checkpointing true \
     --num_train_epochs 2 \
-    --run_name Qwen3-VL-4B-sft \
+    --run_name ObjEmbed-2B-32cards \
     --save_steps 1000 \
     --save_total_limit 2 \
     --max_grad_norm 5 \
@@ -44,5 +45,5 @@ torchrun --nproc_per_node 8 \
     --use_global_caption true \
     --use_two_tokens 2 \
     --use_two_captions true \
-    2>&1 | tee -a ${OUTPUT_DIR_FT}/16cards_log_node_$RANK.txt && echo "Done."
+    2>&1 | tee -a ${OUTPUT_DIR_FT}/log_node_$RANK.txt && echo "Done."
 
